93861c84aef9ee6ed33a2c733e1f89ad8c56bd9c
HasanYousef <hasan.yousef.cs@gmail.com>
2021-12-08 01:46:04 +0200

    working with LOINC API

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,13 +2,23 @@ async function submitFile() {
     const file = document.getElementById("uploadFile").files[0];
     const shouldReplace = document.getElementById("replaceChoice").checked;
     
-    var data = new FormData()
-    data.append('file', file);
-    data.append('shouldReplace', shouldReplace);
-    let response = await fetch('/upload/file', {
-        method: 'POST',
-        body: data
-    });
-    response = await response.text();
-    console.log(response);
+    try {
+        var data = new FormData()
+        data.append('file', file);
+        data.append('shouldReplace', shouldReplace);
+        let response = await fetch('/upload/file', {
+            method: 'POST',
+            body: data
+        });
+        response = await response.text();
+        if(response === 'success') {
+            window.location.href = '/';
+        }
+        else {
+            throw response;
+        }
+    }
+    catch(err) {
+        // code for giving the user a feedback that there is something wrong happened
+    }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
 const path = require('path');
+const fetch = require("node-fetch");
+let base64 = require('base-64');
 
 router.use('/upload', require('./upload'));
 
-router.get('/read', (req, res) => {
+router.get('/read', async (req, res) => {
+    if(req.query.name) {
+
+    }
+    await codeToLongCommonName(code);
     res.sendFile(path.join(__dirname, '../public/read.html'));
 });
 
@@ -14,5 +20,23 @@ router.get('/edit', (req, res) => {
 router.use('/', (req, res) => {
     res.redirect('/read');
 });
+
+async function codeToLongCommonName(code) {
+    try {
+        const headers = new fetch.Headers({
+            method: 'get', 
+            'Authorization': 'Basic ' + base64.encode(`${process.env.LOINC_USERNAME}:${process.env.LOINC_PASSWORD}`),
+        });
+        const response = await fetch(
+            `https://fhir.loinc.org/CodeSystem/$lookup?code=${code}&system=http%3A%2F%2Floinc.org&_format=json`,
+            { headers },
+        );
+        // takes the LONG COMMON NAME from the response
+        return (await response.json()).parameter[1].valueString;
+    }
+    catch(err) {
+        console.log(err)
+    }
+}
 
 module.exports = router;
